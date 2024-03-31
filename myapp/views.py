dc0662e592e22324e1978db50feb2e2157bd87ef
James King <james.king@gmail.com>
2024-03-31 11:29:24 +1100

    Ver 1.2

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import UserSession
 from datetime import datetime
 from django.views.decorators.http import require_POST  # Added import for require_POST
+from django.contrib import messages  # Import the messages module
 
 # Added logout_on_window_close view
 @require_POST
@@ -44,9 +45,13 @@ def register_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        # You may want to add more fields to your registration form
-        user = User.objects.create_user(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('login')  # Redirect to login page after successful registration
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'That username is already taken.', extra_tags='username_taken')
+            return render(request, 'register.html')
+        else:
+            # You may want to add more fields to your registration form
+            user = User.objects.create_user(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('login')  # Redirect to login page after successful registration
     return render(request, 'register.html')
